3a7b5f17dca702886944fdafd7815957e46578c8
Bpost129 <Bpost129@yahoo.com>
2024-06-20 20:38:39 -0400

    change background, projects page layout and text appearance

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -8,37 +8,41 @@ const Projects = () => {
   return (
     <>
       <div className="project-body">
-        <section className="project">
-          <a target="_blank" href="https://green-room-bp.fly.dev/"><img id="project-img" src={greenRoom} alt="1"/></a>
-          <div className="project-desc">
-            <h1>Green Room</h1>
-            <p>Full CRUD application using Django</p>
+        <section>
+          <div className="project" id="ad">
+            <a target="_blank" href="https://green-room-bp.fly.dev/"><img id="project-img" src={greenRoom} alt="1"/></a>
+            <div className="project-desc">
+              <h1>Green Room</h1>
+              <p>Full CRUD application using Django</p>
+            </div>
+            <a id="github-link" target="_blank" href="https://github.com/Bpost129/green-room" style={{backgroundImage: `url(${github})`}}></a>
           </div>
-          <a id="github-link" target="_blank" href="https://github.com/Bpost129/green-room" style={{backgroundImage: `url(${github})`}}></a>
+          <div className="project" id="bd">
+            <a target="_blank" href="https://dittodeal.netlify.app/"><img id="project-img" src={dittoDeal} alt="2"/></a>
+            <div className="project-desc">
+              <h1>DittoDeal</h1>
+              <p>Full MERN stack marketplace application</p>
+            </div>
+            <a id="github-link" target="_blank" href="https://github.com/Bpost129/dittodeal-front-end" style={{backgroundImage: `url(${github})`}}></a>
+          </div>
         </section>
-        <section className="project">
-          <a target="_blank" href="https://dittodeal.netlify.app/"><img id="project-img" src={dittoDeal} alt="2"/></a>
-          <div className="project-desc">
-            <h1>DittoDeal</h1>
-            <p>Full MERN stack marketplace application</p>
+        <section>
+          <div className="project" id="ad">
+            <a target="_blank" href="https://readit-bp.fly.dev/posts"><img id="project-img" src={readit} alt="3"/></a>
+            <div className="project-desc">
+              <h1>Readit</h1>
+              <p>Simple social media app using MongoDB and Javascript</p>
+            </div>
+            <a id="github-link" target="_blank" href="https://github.com/Bpost129/readit" style={{backgroundImage: `url(${github})`}}></a>
           </div>
-          <a id="github-link" target="_blank" href="https://github.com/Bpost129/dittodeal-front-end" style={{backgroundImage: `url(${github})`}}></a>
-        </section>
-        <section className="project">
-          <a target="_blank" href="https://readit-bp.fly.dev/posts"><img id="project-img" src={readit} alt="3"/></a>
-          <div className="project-desc">
-            <h1>Readit</h1>
-            <p>Simple social media app using MongoDB and Javascript</p>
+          <div className="project" id="bd">
+            <a target="_blank" href="https://blackjack-bp.netlify.app/"><img id="project-img" src={blackjack} alt="4"/></a>
+            <div className="project-desc">
+              <h1>Wild West Blackjack</h1>
+              <p>Blackjack game composed of Javascript, HTML and CSS</p>
+            </div>
+            <a id="github-link" target="_blank" href="https://github.com/Bpost129/blackjack-project" style={{backgroundImage: `url(${github})`}}></a>
           </div>
-          <a id="github-link" target="_blank" href="https://github.com/Bpost129/readit" style={{backgroundImage: `url(${github})`}}></a>
-        </section>
-        <section className="project">
-          <a target="_blank" href="https://blackjack-bp.netlify.app/"><img id="project-img" src={blackjack} alt="4"/></a>
-          <div className="project-desc">
-            <h1>Wild West Blackjack</h1>
-            <p>Blackjack game composed of Javascript, HTML and CSS</p>
-          </div>
-          <a id="github-link" target="_blank" href="https://github.com/Bpost129/blackjack-project" style={{backgroundImage: `url(${github})`}}></a>
         </section>
       </div>
     </>
