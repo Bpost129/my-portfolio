3e771a1419ac910040c36a2548d688a60a25179b
Bpost129 <Bpost129@yahoo.com>
2025-01-06 22:02:51 -0500

    fix navbar movement, add resume link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,15 @@
 import { NavLink } from "react-router-dom"
 
+import doc from '../../assets/docie2.png'
+
 const NavBar = () => {
   return (
     <nav>
-      <p><NavLink to="/">Home</NavLink></p>
-      <p><NavLink to="/projects">Projects</NavLink></p>
+      <p><img src={doc} alt="resume" className='resume-img' /></p>
+      <div className="tabs">
+        <p><NavLink to="/">Home</NavLink></p>
+        <p><NavLink to="/projects">Projects</NavLink></p>
+      </div>
     </nav>
   )
 }
