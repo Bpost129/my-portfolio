a5510b5d0e1fb6d11d5d6bf8fa63d1f1f8a95f5d
Bpost129 <Bpost129@yahoo.com>
2025-01-07 01:50:58 -0500

    add new background image, alter navbar, skills list, and contact footer stylings

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,12 @@
 import { NavLink } from "react-router-dom"
 
-import doc from '../../assets/docie2.png'
+// import doc from '../../assets/docie1.png'
 
 const NavBar = () => {
   return (
     <nav>
-      <p><img src={doc} alt="resume" className='resume-img' /></p>
-      <div className="tabs">
-        <p><NavLink to="/">Home</NavLink></p>
-        <p><NavLink to="/projects">Projects</NavLink></p>
-      </div>
+      <p><NavLink to="/">Home</NavLink></p>
+      <p><NavLink to="/projects">Projects</NavLink></p>
     </nav>
   )
 }
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,15 +6,15 @@ const Skills = () => {
       <h1>Skills</h1>
       <div className="skills-list">
         <div>
-          <p><img src='logo-html.png' style={{height: '100px', width: '125px'}} />HTML5</p>
           <p><img src='logo-css.png' style={{height: '100px', width: '125px', margin: '0 10px'}} />CSS3</p>
+          <p><img src='logo-html.png' style={{height: '100px', width: '125px'}} />HTML5</p>
         </div>
         <div>
           <p><img src='logo-javascript.png' style={{height: '100px', width: '125px'}} />JavaScript</p>
           <p><img src='logo-python.png' style={{height: '100px', width: '125px'}} />Python</p>
         </div>
         <div>
-          <p><img src='logo-react.png' style={{height: '100px', width: '175px'}} />React</p>
+          <p><img src='logo-react.png' style={{height: '100px', width: '175px', background: 'radial-gradient(circle, rgba(0,0,0,1) 0%, rgb(251,255,241,1) 50%)'}} />React</p>
           <p><img src='logo-dj.png' style={{height: '100px', width: '110px', margin: '0 10px'}} />Django</p>
         </div>
         <div>
