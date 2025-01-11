b02c5c44b6f7057bdb1f3f4278b5997cba8aa46b
Bpost129 <Bpost129@yahoo.com>
2025-01-11 00:37:05 -0500

    alter project technologies, add shadow to project image container

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -9,7 +9,7 @@ const projectsData = [
     id: 1,
     title: 'DittoDeal',
     description: 'Full MERN stack marketplace application with a pokemon theme. As you\'ll see you can sell more than just pokemon cards, but who wants that?',
-    tech: ['CSS', 'HTML', 'JavaScript', 'Git', 'Mongoose', 'MongoDB', 'Express', 'Node.js', 'React', 'JSON Web Tokens', 'Netlify'],
+    tech: ['CSS', 'HTML', 'JavaScript', 'Mongoose', 'MongoDB', 'Express', 'Node.js', 'React', 'JSON Web Tokens'],
     image: dittoDeal,
     repositoryLink: 'https://github.com/Bpost129/dittodeal-front-end',
     deploymentLink: 'https://dittodeal.netlify.app/'
@@ -18,7 +18,7 @@ const projectsData = [
     id: 2,
     title: 'Green Room',
     description: 'Full CRUD application using Django',
-    tech: ['CSS', 'HTML', 'Python', 'Git', 'PostgreSQL', 'Django', 'Docker'],
+    tech: ['CSS', 'HTML', 'Python', 'PostgreSQL', 'Django', 'Docker'],
     image: greenRoom,
     repositoryLink: 'https://github.com/Bpost129/green-room',
     deploymentLink: 'https://green-room-bp.fly.dev/'
@@ -27,7 +27,7 @@ const projectsData = [
     id: 3,
     title: 'Readit',
     description: 'Simple social media app using Javascript and MongoDB',
-    tech: ['CSS', 'HTML', 'JavaScript', 'Git', 'Mongoose', 'MongoDB', 'Express', 'Passport', 'Node.js'],
+    tech: ['JavaScript', 'EJS', 'Mongoose', 'MongoDB', 'Express', 'Passport', 'Node.js'],
     image: readit,
     repositoryLink: 'https://github.com/Bpost129/readit',
     deploymentLink: 'https://readit-bp.fly.dev/posts'
@@ -36,7 +36,7 @@ const projectsData = [
     id: 4,
     title: 'Wild West Blackjack',
     description: 'Blackjack game composed of CSS, HTML, and Javascript',
-    tech: ['CSS', 'HTML', 'JavaScript', 'Git'],
+    tech: ['CSS', 'HTML', 'JavaScript'],
     image: blackjack,
     repositoryLink: 'https://github.com/Bpost129/blackjack-project',
     deploymentLink: 'https://blackjack-bp.netlify.app/'
